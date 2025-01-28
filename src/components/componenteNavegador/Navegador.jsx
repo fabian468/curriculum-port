16d8f8986129d28c724b67720d676ce0e7396806
fabian468 <fabianarandag@hotmail.com>
2025-01-28 12:45:32 -0300

    creando contraste nav

--- src/components/componenteNavegador/Navegador.jsx
+++ src/components/componenteNavegador/Navegador.jsx
@@ -4,7 +4,7 @@ import { FaHome } from "react-icons/fa";
 
 function Navegador() {
     return (
-        <ul className="flex text-sm w-11/12 md:text-md md:w-1/3 m-auto font-mono  justify-between items-center  ">
+        <ul className="flex text-sm w-11/12 md:text-md md:w-1/3 p-2 opacity-85 rounded-md bg-black m-auto font-mono  justify-between items-center  ">
             <BotonNavegador ad={<FaHome />} href={"#home"} />
             <BotonNavegador ad={"Proyectos"} href={"#proyectos"} />
             <BotonNavegador ad={"Quien soy"} href={"#quienSoy"} />
